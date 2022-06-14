5de004997f1f2b8b30dc1e2abd10d9a925c92e23
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-14 16:33:22 +0300

    commit tree 4e0dcba924d80d69ee7b94921e348fa36fe49285

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    changeDOM ()
+});
+
+function changeDOM(){
+    document.getElementById( "text")
+    .innerHTML = "This is really cool!";
+}
